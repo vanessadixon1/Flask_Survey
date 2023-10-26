1de9633262b51d82e894ef7ad73485a9761222d4
Vanessa Dixon <53667448+vanessadixon1@users.noreply.github.com>
2023-10-26 19:19:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
-from flask import Flask, request, render_template, redirect, flash, jsonify
+from flask import Flask, request, render_template, redirect, flash, session
 
-import socket
 
 import surveys
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secet"
 
-
-res = []
-iden = 0
 question_len = 0
-socket.setdefaulttimeout(5)
 
 @app.route("/")
 def home():
    info = surveys.personality_quiz
+   if not session.get("response"):
+        session["response"] = []
 
    return render_template("homepage.html", info = info)
 
 @app.route("/questions/<id>")
 def question(id):
-    global iden
     global question_len
     info = surveys.personality_quiz
     question_len = len(info.questions)
@@ -29,12 +25,8 @@ def question(id):
     url_id = int(request.url.split("/")[-1])
     
     if url_id > question_len:
-        print(res)
-        indx = 0
-        iden = indx
-        res.clear()
         flash("Ivalid Pick!", 'error')
-        return redirect(f"/questions/{len(res)}")
+        return redirect(f"/questions/{len(session['response'])}")
 
     indx = int(id)
     
@@ -45,20 +37,15 @@ def question(id):
 @app.route("/answer", methods=['POST'])
 def answer():
     try:
-        global iden
-        if request.form["q"]:
+        if request.form.get("q", False):
+            s = session["response"]
+            s.append(request.form["q"])
+            session["response"] = s
 
-            res.append(request.form["q"])
+        while question_len > len(session["response"]):
+            return redirect(f"/questions/{len(session['response'])}")
 
-        iden += 1
-        while question_len > iden:
-            return redirect(f"/questions/{iden}")
-
-        iden=0
-        res.clear()
         flash("Thank You!", 'success')
         return redirect("/")  
     except:
         raise
-    
-  
